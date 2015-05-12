d84c927b9c5839c6d7ad18fc9693cc3c32930e6b
Elliott Lester <lester.elliott@gmail.com>
2015-05-12 19:21:17 +1000

    Add q4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #![feature(core)]
 #![feature(test)]
+#![feature(convert)]
 
 extern crate test;
 
 pub mod q1;
 pub mod q2;
 pub mod q3;
+pub mod q4;
 
 #[cfg(not(test))]
 fn main() {
     println!("Q1: {:?}" ,q1::soln());
     println!("Q2: {:?}" ,q2::soln());
     println!("Q3: {:?}" ,q3::soln(600851475143));
+    println!("Q4: {:?}" ,q4::soln());
 }
 
 #[cfg(test)]
@@ -22,23 +25,45 @@ pub fn q1() {assert!(q1::soln() == 233168)}
 pub fn q2() {assert!(q2::soln() == 4613732)}
 #[test]
 pub fn q3() {assert!(q3::soln(600851475143) == (6857,6857))}
+#[test]
+pub fn q4() {assert!(q4::soln() == (906609,906609))}
 
 #[bench]
-fn iter_primes(b: &mut Bencher) {
+fn loop_q3(b: &mut Bencher) {
     b.iter(|| {
         test::black_box(
-            for _ in 0..100 {
+            for _ in 0..5 {
                 q3::largest_prime_factor(600851475143);
             }
         )
     })
 }
 #[bench]
-fn fold_primes(b: &mut Bencher) {
+fn iter_q3(b: &mut Bencher) {
     b.iter(|| {
         test::black_box(
-            for _ in 0..100 {
+            for _ in 0..5 {
                 q3::largest_prime_factor2(600851475143);
+            }
+        )
+    })
+}
+#[bench]
+fn loop_q4(b: &mut Bencher) {
+    b.iter(|| {
+        test::black_box(
+            for _ in 0..2 {
+                q4::loop_soln();
+            }
+        )
+    })
+}
+#[bench]
+fn iter_q4(b: &mut Bencher) {
+    b.iter(|| {
+        test::black_box(
+            for _ in 0..2 {
+                q4::iter_soln();
             }
         )
     })
--- src/q4.rs
+++ src/q4.rs
@@ -1,13 +1,28 @@
-#![feature(convert)]
+use std;
 
-pub fn is_palindrome(input:&str) -> bool {
+fn is_palindrome(input:&str) -> bool {
     let forward = input.chars().take(input.len()/2);
     let reverse = input.chars().rev().take(input.len()/2);
     let mut both    = forward.zip(reverse);
     both.all( |(a,b)| {a == b})
 }
 
-pub fn main() {
+pub fn iter_soln() -> usize {
+        (0..999)
+        .rev()
+        .flat_map(|x|
+                  std::iter::repeat(x)
+                  .zip((1..x+1).rev())
+                 )
+        .map(|(x,y)| x * y)
+        .filter(|x|
+                is_palindrome(format!("{}",x).as_str()))
+        .max()
+        .unwrap_or(0)
+}
+
+
+pub fn loop_soln() -> usize {
     let mut high = 0;
     let mut test;
     for x in 0..999 {
@@ -18,6 +33,9 @@ pub fn main() {
             }
         }
     }
-    println!("test {:?}",high);
+    high
+}
 
+pub fn soln() -> (usize,usize) {
+    (iter_soln(),loop_soln())
 }
